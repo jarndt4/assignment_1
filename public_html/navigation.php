1d96c6c52fe3aaa85d451845ee406c124a7cab48
Prince Krah <prince.krah@my.simpson.edu>
2018-10-31 22:50:24 -0500

    my newest changes

---
+++ public_html/navigation.php
@@ -0,0 +1,28 @@
+ <nav>
+        <ul>
+            <li>
+                <a href="index.php">Home Page</a>
+            </li>
+            
+            <li>
+                <a href="about.php">About</a>
+            </li>
+            
+            <li>
+                <a href="beermenu.php">Drink Menu</a>
+            </li>
+            
+            <li>
+                <a href="foodmenu.php">Food Menu</a>
+            </li>
+            
+            <li>
+                <a href="events.php">Upcoming events</a>
+            </li>
+            
+            <li>
+                <a href="staff.php">Meet the staff</a>
+            </li>
+        </ul>
+    </nav>
+    
